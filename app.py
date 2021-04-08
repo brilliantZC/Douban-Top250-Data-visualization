68f63545a025130235caabc66ca58ddd0e84c2a2
Brilliant ZC <2411464044@qq.com>
2021-04-08 15:39:56 +0800

    相关网页完善和wordcloud制作，生成词云图片

--- app.py
+++ app.py
@@ -28,7 +28,22 @@ def showmovie():
 
 @app.route('/score')
 def showscore():
-    return render_template("score.html")
+    score = [] # 评分
+    num = []   # 评分的电影数
+    conn = sqlite3.connect("movie.db")
+    cur = conn.cursor()
+    sql = "select score,count(score) from movie250 group by score"
+    data = cur.execute(sql)
+    for item in data:
+        score.append(item[0])
+        num.append(item[1])
+    cur.close()
+    conn.close()
+    return render_template("score.html", score=score, num=num)
+
+@app.route('/word')
+def showword():
+    return render_template("word.html")
 
 @app.route('/team')
 def showteam():
